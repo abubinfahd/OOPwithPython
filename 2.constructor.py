1018c74ab563d22c74bb45330e8a076122899140
Abu Bin Fahd <192040@ku.ac.bd>
2023-06-27 17:10:34 +0600

    class attributes

--- 2.constructor.py
+++ 2.constructor.py
@@ -1,13 +1,15 @@
 # Create class:
 class Item:
+    # Class attributes:
+    pay_rate = 0.8 # The pay rate after 20% discount
     # Constructor:
     def __init__(self, name: str, price: float, quantity=0):
 
-        # Run validation to receive arguments
+        # Run validation to receive arguments:
         assert price >= 0, f"Price {price} is not greater than zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
 
-        # Assign self object
+        # Assign self object:
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -15,14 +17,23 @@ class Item:
     # Methods:
     def calculate_total_price(self):
         return self.price * self.quantity
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
 # Create an instance of a class   
 item1 = Item('Phone', 10000, 5) # try negative numbner of price or quantity
 # Create second instance(one can create as much as want):
 item2 = Item('Laptop', 30000, 3)
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+# print(Item.pay_rate)
+# print(item1.pay_rate)
+# print(item2.pay_rate)
 
+print(Item.__dict__) # All the attributes for class level
+print("\n")
+print(item1.__dict__) # All thye attributes for instance level
+
+item1.apply_discount()
+print(item1.price)
 
 
