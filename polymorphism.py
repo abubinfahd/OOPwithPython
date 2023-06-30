6f5dc7780753067556bdfd701d2eb5715bb780a5
Abu Bin Fahd <192040@ku.ac.bd>
2023-06-30 21:16:00 +0600

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,12 @@
+# use of a single entity to different types in different senarios
+# it referce to many forms
+# example
+
+name = "Abu Bin Fahd"
+print(len(name))
+
+name_list = ['Abu', 'Bin', 'Fahd']
+print(len(name_list))
+
+# That's polymorphism in action, a single function does know
+# how to handle differents kinds of objects  as expected
